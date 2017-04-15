80f5e063e7357c978e86ede32280cc29937fb959
Deyvid Bustos Marquez <marquez.deyvid.88@gmail.com>
2017-04-15 16:21:25 -0400

    New

--- js/main.js
+++ js/main.js
@@ -1,31 +1,105 @@
 var notes = $('.sound');
-var note = [0,0,0,3,5,0,0,0,3,5,3,3,2,2,1,1,0,0,0,0,2,4,0,0,0,2,4,7,2,7,6,5,3];
-var tempo = [1,1,1,2,2,1,1,1,2,1,1,1,1,2,1,2,1,1,1,1,2,2,1,1,1,1,1,1,1,1,1,1,1];
+
+// do 0
+// re 1
+// mi 2
+// fa 3
+// sol 4
+// la 5
+// si 6
+// do+ 7
+
+var note1 = [0,0,0,3,5,  0,0,0,3,5,  3,3,2,2,1,1,0,  0,0,0,2,4,  0,0,0,2,4,  7,7,6,5,4,3,1,0];
+var tempo1 = [1,1,1,2,3,  1,1,1,2,3,  1,1,1,1,1,1,3,  1,1,1,2,3,  1,1,1,2,3,  2,1,1,1,1,1,1,1];
+
+var note2 = [2,2,2,  2,2,2,  2,4,0,1,2,  3,3,3,  3,2,2,  2,1,1,2,1,4,  2,2,2,  2,2,2,  2,4,0,1,2,  3,3,3,  3,2,2,  4,4,3,1,0];
+var tempo2 = [1,1,2,  1,1,2,  1,1,1,1,4,  1,1,2,  1,1,2,  1,1,1,1,2,2,  1,1,2,  1,1,2,  1,1,1,1,4,  1,1,2,  1,1,2,  1,1,1,1,4];
+
+var note3 = [0,0,1,0,3,2,  0,0,1,0,4,3,  0,0,7,5,3,3,2,1,  6,6,5,3,4,3];
+var tempo3 = [2,1,2,2,2,3,  2,1,2,2,2,3,  2,1,2,2,2,2,2,3,  2,1,2,2,2,3];
+
+var note4 = [0,0,4,4,5,5,4,5,5,2,2,1,1,0,  4,4,3,3,2,2,1,4,4,3,3,2,2,1,  0,0,4,4,5,5,4,3,3,2,2,1,1,0];
+var tempo4 =[2,2,2,2,2,2,4,2,2,2,2,2,2,3,  2,2,2,2,2,2,4,2,2,2,2,2,2,3,  2,2,2,2,2,2,4,2,2,2,2,2,2,3];
 
 $('.note').click(function() {
   var num = parseInt(this.id);
   play(num);
 });
 
-$('.btn').click(function() {
+$('#btn1').click(function() {
   var count = 0;
-  for(var i= 0; i < note.length; i++) {
+  for(var i= 0; i < note1.length; i++) {
     setTimeout(function(sound) {
-      play(note[sound]);
-    }, rhythm(i) * 350, i);
+      play(note1[sound]);
+    }, rhythm1(i) * 350, i);
+  }
+});
+
+$('#btn2').click(function() {
+  var count = 0;
+  for(var i= 0; i < note2.length; i++) {
+    setTimeout(function(sound) {
+      play(note2[sound]);
+    }, rhythm2(i) * 350, i);
+  }
+});
+
+$('#btn3').click(function() {
+  var count = 0;
+  for(var i= 0; i < note3.length; i++) {
+    setTimeout(function(sound) {
+      play(note3[sound]);
+    }, rhythm3(i) * 350, i);
+  }
+});
+
+$('#btn4').click(function() {
+  var count = 0;
+  for(var i= 0; i < note4.length; i++) {
+    setTimeout(function(sound) {
+      play(note4[sound]);
+    }, rhythm4(i) * 350, i);
   }
 });
 
 function play(num) {
   notes[num].pause();
   notes[num].currentTime = 0;
-  notes[num].play();  
+  notes[num].play();
+  $("#" + num).css("font-weight", "bold");
+  setTimeout(function() {
+    $("#" + num).css("font-weight", "normal");
+  }, 300);
 }
 
-function rhythm(num) {
+function rhythm1(num) {
   var count = 0;
   for (var i = 0; i < num ; i++) {
-    count = count + tempo[i];
+    count = count + tempo1[i];
+  }
+  return count;
+}
+
+function rhythm2(num) {
+  var count = 0;
+  for (var i = 0; i < num ; i++) {
+    count = count + tempo2[i];
+  }
+  return count;
+}
+
+function rhythm3(num) {
+  var count = 0;
+  for (var i = 0; i < num ; i++) {
+    count = count + tempo3[i];
+  }
+  return count;
+}
+
+function rhythm4(num) {
+  var count = 0;
+  for (var i = 0; i < num ; i++) {
+    count = count + tempo4[i];
   }
   return count;
 }
